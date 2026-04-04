37a86e14d69d274fb9aef4177a94f6dfed22eb25
Adpros7 <pranit.advik@gmail.com>
2026-04-04 16:40:40 -0400

    Refactor tkify.py to use a global root window and atexit registration for running the GUI.

--- tkify.py
+++ tkify.py
@@ -1,42 +1,22 @@
-import sys
+import atexit
+import builtins
 import tkinter as tk
 
-
-class TKInput:
-    def __init__(self, runCheck=True, windowSize=(1000, 1000)) -> None:
-        self.entries = []
-        self.root = tk.Tk()
-        self.root.geometry(f"{windowSize[0]}x{windowSize[1]}")
-        self.tkUseCheck() if runCheck else None
-
-    def tkinput(self, prompt):
-        self.entries.append(tk.Entry(self.root, textvariable=tk.StringVar(self.root, prompt)))
-
-    def tkUseCheck(self):
-        with open(__file__, "r") as f:
-            cur = f.read()
-
-        if cur.find("input(") == cur.find("tkinput("):
-            return
-
-        else:
-            print("replacing tkinput() with tkinput(). You will need to restart the program.")
-            with open(__file__, "w") as f:
-                f.write(cur.replace("tkinput(", "tkinput("))
-
-            sys.exit()
-
-    def render(self):
-        for entry in self.entries:
-            entry.pack()
-
-        self.root.mainloop()
+objects = []
 
 
-if __name__ == "__main__":
-    inp = TKInput(False)
-    inp.tkinput("test")
-    inp.tkinput("test2")
-    inp.tkinput("test3")
-    inp.tkinput("test4")
-    inp.render()
+
+root = tk.Tk()
+root.geometry("500x500")
+def add_object(obj: str):
+    objects.append((obj, tk.Entry(root, textvariable=tk.StringVar(root, value=obj))))
+
+def run_code():
+    global objects
+    for i in objects:
+        tk.Label(root, text=i[0]).pack(justify="right")
+        tk.Entry(root, textvariable=i[1]).pack(justify="left")
+
+    root.mainloop()
+
+atexit.register(run_code)
